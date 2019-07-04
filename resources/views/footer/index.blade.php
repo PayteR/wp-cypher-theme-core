5750ed8375c19fd47a450ae0163a751b09c50670
Peter PayteR Gašparík <payter86@gmail.com>
2019-07-04 18:58:33 +0200

    fixes

--- resources/views/footer/index.blade.php
+++ resources/views/footer/index.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   @include('footer.before.')
   <div class="container footer-container">
-    <span class="footer-copyright">Copyright &copy; {{ get_bloginfo('name', 'display') }} 2015-{{ date("Y") }}</span>
+    <span class="footer-copyright">Copyright &copy; {{ get_bloginfo('name', 'display') }} {{ date("Y") }}</span>
     @php dynamic_sidebar('sidebar-footer') @endphp
   </div>
   @include('footer.after.')
